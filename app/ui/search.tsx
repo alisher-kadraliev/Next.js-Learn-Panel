b0920f097637abe0f9b7b8577008597ab7843a13
Alisher Kadraliev <alisherkadraliev@gmail.com>
2024-06-06 14:29:43 +0300

    added search and pagination with fallbacks

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,14 +1,33 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { usePathname, useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const pathname = usePathname()
+  const { replace } = useRouter()
+  const searchParams = useSearchParams()
+  const handleSearch = useDebouncedCallback((temp: string) => {
+    console.log(`Searching .. ${temp}`);
+    const params = new URLSearchParams(searchParams)
+    params.set('page','1')
+    if (temp) {
+      params.set('query', temp)
+    } else {
+      params.delete('query')
+    }
+    replace(`${pathname}?${params.toString()}`)
+  }, 300)
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
       <input
+        defaultValue={searchParams.get('query')?.toString()}
+        onChange={e => { handleSearch(e.target.value) }}
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
       />
